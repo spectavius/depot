2edf340c1fade00dcd665bbca6292679ec82deed
your mom <spectre@sticky.clownjuice.com>
2011-12-07 11:02:54 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '63a8b031a4cf4dd2e67022baa14c4f6b8705e67789a49174a9e4d7f510930deb3874325883e3221ad6cf8016ebd5e8774246289936e95adac83ed3bcff5a9f8f'
